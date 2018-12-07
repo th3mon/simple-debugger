540c7263ac4ee7d97ed5c4fe79fd000ef05087ff
th3mon <p.szelenberger@gmail.com>
2018-12-07 15:05:25 +0100

    Fix vulnerabilities
    Update dependencies
    
    
    Upgrade node version in circle-ci configuration
    
    
    Add mode option
    
    
    Remove deprecated or removed plugins
    
    
    Update packages
    
    
    Update configs

--- config/dev.js
+++ config/dev.js
@@ -6,77 +6,74 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 
-module.exports = function () {
-  return {
-    entry: {
-      [libraryName]: './src/index.js'
+module.exports = () => ({
+  mode: 'development',
+  entry: {
+    [libraryName]: './src/index.js'
+  },
+  devtool: 'cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+    library: {
+      root: 'SimpleDebugger',
+      amd: libraryName,
+      commonjs: libraryName
     },
-    devtool: 'cheap-module-source-map',
-    output: {
-      path: path.resolve(__dirname, '../dist'),
-      filename: '[name].js',
-      library: {
-        root: 'SimpleDebugger',
-        amd: libraryName,
-        commonjs: libraryName
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    sourceMapFilename: '[name].map'
+  },
+  module: {
+    rules: [{
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
       },
-      libraryTarget: 'umd',
-      umdNamedDefine: true,
-      sourceMapFilename: '[name].map'
-    },
-    module: {
-      rules: [
-        {
-          enforce: 'pre',
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'eslint-loader'
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
           }
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: 'css-loader'
-          })
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [ 'css-loader', 'sass-loader' ]
-          })
         }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin({
-        filename: '[name].css'
-      }),
-      new HtmlWebpackPlugin({
-        title: 'SimpleDebugger',
-        alwaysWriteToDisk: true
-      }),
-      new HtmlWebpackHarddiskPlugin(),
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin()
-    ],
-    devServer: {
-      contentBase: './dist',
-      port: 7777,
-      host: 'localhost',
-      hot: true,
-      noInfo: false,
-      stats: 'minimal'
-    }
-  };
-};
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'SimpleDebugger',
+      alwaysWriteToDisk: true
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  devServer: {
+    contentBase: './dist',
+    port: 7777,
+    host: 'localhost',
+    hot: true,
+    noInfo: false,
+    stats: 'minimal'
+  }
+});
--- config/prod.js
+++ config/prod.js
@@ -5,88 +5,78 @@ const libraryName = 'simple-debugger';
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = function () {
-  return {
-    entry: {
-      [libraryName]: './src/index.js'
+module.exports = () => ({
+  mode: 'production',
+  entry: {
+    [libraryName]: './src/index.js'
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].min.js',
+    library: {
+      root: 'SimpleDebugger',
+      amd: libraryName,
+      commonjs: libraryName
     },
-    output: {
-      path: path.resolve(__dirname, '../dist'),
-      filename: '[name].min.js',
-      library: {
-        root: 'SimpleDebugger',
-        amd: libraryName,
-        commonjs: libraryName
-      },
-      libraryTarget: 'umd',
-      sourceMapFilename: '[name].map'
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['env']
-            }
+    libraryTarget: 'umd',
+    sourceMapFilename: '[name].map'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
           }
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [{
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
               loader: 'css-loader',
               options: {
                 minimize: true,
                 sourceMap: true
               }
-            }]
-          })
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            fallback: 'style-loader',
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  minimize: true,
-                  sourceMap: true
-                }
-              },
-              'sass-loader'
-            ]
-          })
-        }
-      ]
-    },
-    plugins: [
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        beautify: false,
-        mangle: {
-          screw_ie8: true,
-          keep_fnames: true
-        },
-        compress: {
-          screw_ie8: true
-        },
-        comments: false
-      }),
-      new ExtractTextPlugin({
-        filename: '[name].min.css'
-      }),
-      new HtmlWebpackPlugin({
-        title: 'SimpleDebugger',
-        filename: path.join(__dirname, '/../index.html')
-      })
+            }
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                minimize: true,
+                sourceMap: true
+              }
+            },
+            'sass-loader'
+          ]
+        })
+      }
     ]
-  };
-};
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].min.css'
+    }),
+    new HtmlWebpackPlugin({
+      title: 'SimpleDebugger',
+      filename: path.join(__dirname, '/../index.html')
+    })
+  ]
+});
